0c7e02147387cee49c60593307c48a6b0cec9a80
Unknown <15979180165@163.com>
2017-05-26 14:15:19 +0800

    添加了注释
    添加了注释

--- Stiching1.0.py
+++ Stiching1.0.py
@@ -14,6 +14,7 @@ global dst_rows,dst_cols
 global kp1,kp2
 global des1,des2
 global matches,matchesMask
+import time
 def readimgs(*imgs):
     read_imgs =[]
     for img in imgs:
@@ -26,88 +27,107 @@ def FeaturesAndMatch(srcimg,dstimg,hessianThreshold):
     global kp1, kp2
     global des1, des2
     global matches, matchesMask
-    surf = xf2d.SURF_create(hessianThreshold)
-    kp1,des1 = surf.detectAndCompute(srcimg,None)
-    kp2,des2 = surf.detectAndCompute(dstimg,None)
+    surf = xf2d.SURF_create(hessianThreshold)#创建SURF对象
+    kp1,des1 = surf.detectAndCompute(srcimg,None)#用SURF方法找出源图像特征点
+    kp2,des2 = surf.detectAndCompute(dstimg,None)#用SURF方法找出目标图像特征点
     FLANN_INDEX_KDTREE = 0
     index_params = dict(algorithm=FLANN_INDEX_KDTREE, tree=5)
     search_params = dict(checks=50)
-    flann = cv2.FlannBasedMatcher(index_params, search_params)
-    matches = flann.knnMatch(des1, des2, k=2)
+    flann = cv2.FlannBasedMatcher(index_params, search_params)#创建flann对象，用flann方法进行特征点匹配
+    matches = flann.knnMatch(des1, des2, k=2)#找到两个匹配点，一个是距离最优点，另一个是次优点
     matchesMask = [[0, 0] for i in range(len(matches))]
     for i, (m, n) in enumerate(matches):
-        if m.distance < 0.7 * n.distance:
+        if m.distance < 0.7 * n.distance:#最优点距离小于0.7倍的次优点视为好的匹配点
             matchesMask[i] = [1, 0]
     draw_params = dict(matchColor=(0, 255, 0),
                        singlePointColor=(255, 0, 0),
                        matchesMask=matchesMask,
                        flags=0)
+    #画出两张图的匹配点图像
     Matchimg = cv2.drawMatchesKnn(srcimg, kp1, dstimg, kp2, matches, None, **draw_params)
     plt.figure(1)
     plt.imshow(Matchimg)
 def PerspectivetransFormationMatrix(srcimg,dstimg):
+    #计算透视变换矩阵
+    #利用优质的匹配点进行计算，首先要将特征点化坐标找出，化为numpy数组
     src_pts = np.float32([kp1[m.queryIdx].pt for i, (m, n) in enumerate(matches)]).reshape(-1, 1, 2)
     dst_pts = np.float32([kp2[m.trainIdx].pt for i, (m, n) in enumerate(matches)]).reshape(-1, 1, 2)
+    #进行透视变换矩阵计算，采用RANSAC方法
     M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
     pts = np.float32([[0, 0], [0, src_rows - 1], [src_cols - 1, src_rows - 1], [src_cols - 1, 0]]).reshape(-1, 1, 2)
     dst = cv2.perspectiveTransform(pts, M)
     dstimg_copy = dstimg.copy()
+    #画出将要拼接的部分
     pictures = cv2.polylines(dstimg_copy, [np.int32(dst)], True, 255, 3, cv2.LINE_AA)
+    #进行变换，得到变换后图像
     transformedimg = cv2.warpPerspective(srcimg, M, (src_cols + dst_cols, max(src_rows, dst_rows)))
     plt.figure(2)
     plt.imshow(pictures)
     plt.figure(3)
     plt.imshow(transformedimg)
     return pictures,transformedimg
+#进行图像拼接，这个方法是直接进行拼接
 def StichImg(dstimg,transformedimg):
     global src_rows, src_cols
     transformedimg[0:src_rows,0:src_cols] = dstimg
+    img = cv2.cvtColor(transformedimg,cv2.COLOR_RGB2GRAY)
+    img = cv2.threshold(img,)
     plt.figure(4)
     plt.imshow(transformedimg)
-def StichImgbypyramid(dstimg,transformedimg):
-    gpA = Gaussian_pyramid(dstimg)
-    gpB = Gaussian_pyramid(transformedimg)
-    lpA = Laplacian_pyramid(gpA)
-    lpB = Laplacian_pyramid(gpB)
-    LS_add(lpA, lpB)
-def Gaussian_pyramid(img):
+
+
+#用金字塔方法进行拼接，希望能平滑拼接缝隙
+def StichImgbypyramid(dstimg,transformedimg,layer):
+    gpA = Gaussian_pyramid(dstimg,layer)
+    gpB = Gaussian_pyramid(transformedimg,layer)
+    lpA = Laplacian_pyramid(gpA,layer)
+    lpB = Laplacian_pyramid(gpB,layer)
+    LS_add(lpA, lpB,layer)
+#首先进行高斯金字塔计算
+def Gaussian_pyramid(img,layer):
     G = img.copy()
     gp = [G]
-    for i in range(5):
+    for i in range(layer):
         G = cv2.pyrDown(G)
         gp.append(G)
     return gp
-def Laplacian_pyramid(gp):
-    lp = [gp[5]]
-    for i in range(5, 1, -1):
+#再通过高斯金字塔计算拉普拉斯金字塔
+def Laplacian_pyramid(gp,layer):
+    lp = [gp[layer]]
+    for i in range(layer, 1, -1):
         GE = cv2.pyrUp(gp[i])
         h, w, c = gp[i - 1].shape
         L = cv2.subtract(gp[i - 1], GE[0:h, 0:w])
         lp.append(L)
     return lp
-def LS_add(lpA,lpB):
+#将每层金字塔融合再求和
+def LS_add(lpA,lpB,layer):
     LS = []
     for la, lb in zip(lpA, lpB):
         h, w, c = la.shape
         lb[0:h, 0:w] = la
         LS.append(lb)
     ls_ = LS[0]
-    for i in range(1, 5):
+    for i in range(1, layer):
         ls_ = cv2.pyrUp(ls_)
         h, w, c = LS[i].shape
         ls_ = cv2.add(ls_[0:h, 0:w], LS[i])
     plt.figure(5)
     plt.imshow(ls_)
 def main():
+    starttime = time.time()
     global src_rows, src_cols
     global dst_rows, dst_cols
-    srcimg,dstimg = readimgs('test2.jpg','test1.jpg')
+    srcimg,dstimg = readimgs('test6.jpg','test5.jpg')
     src_rows,src_cols,c1 = srcimg.shape
     dst_rows,dst_cols,c2 = dstimg.shape
-    FeaturesAndMatch(srcimg,dstimg,2000)
+    FeaturesAndMatch(srcimg,dstimg,3000)
     pictures, transformedimg = PerspectivetransFormationMatrix(srcimg,dstimg)
     StichImg(dstimg,transformedimg)
-    StichImgbypyramid(dstimg,transformedimg)
+    StichImgbypyramid(dstimg,transformedimg,5)
+    stoptime = time.time()
+    runtime = stoptime - starttime
+    print(runtime)
     plt.show()
 if __name__ == '__main__':
     main()
